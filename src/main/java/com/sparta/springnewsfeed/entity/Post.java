ab8d33021820249ba4ec1f63c3425bebd60d9c88
Woojin1123 <boom175839@gmail.com>
2024-09-02 17:36:22 +0900

    Feat : Timestamped 추가 & Post Entity 추가

--- src/main/java/com/sparta/springnewsfeed/entity/Post.java
+++ src/main/java/com/sparta/springnewsfeed/entity/Post.java
@@ -1,25 +1,22 @@
 package com.sparta.springnewsfeed.entity;
 
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class User extends Timestamped{
-
+public class Post extends Timestamped{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String email;
-    private String password;
-    private String nickname;
+    private String title;
 
-    private String introduce;
+    @ManyToOne
+    private User user;
+
 }
