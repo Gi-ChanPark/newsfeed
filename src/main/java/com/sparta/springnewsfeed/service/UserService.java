9bd3261ea6a8cd3a364b6af3a66b5d0858b2ecf2
Gi-ChanPark <parkland0859@gmail.com>
2024-09-02 20:44:48 +0900

    Feat : UserController, UserService 추가

--- src/main/java/com/sparta/springnewsfeed/service/UserService.java
+++ src/main/java/com/sparta/springnewsfeed/service/UserService.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void signup(UserSignupRequestDto requestDto) {
