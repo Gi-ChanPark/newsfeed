a191c5f8fc0d070dc52d4b24fbf61ba78187a6c9
Woojin1123 <boom175839@gmail.com>
2024-09-02 17:38:36 +0900

    Feat : Post Entity 컬럼 추가

--- src/main/java/com/sparta/springnewsfeed/entity/Post.java
+++ src/main/java/com/sparta/springnewsfeed/entity/Post.java
@@ -9,12 +9,16 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @NoArgsConstructor
-public class Post extends Timestamped{
+public class Post extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "content")
+    private String content;
 
     @ManyToOne
     private User user;
