22bf3067d8d4dcc73d1c31041c7c8976e426a355
Woojin1123 <boom175839@gmail.com>
2024-09-03 10:32:08 +0900

    Feat : 게시물 조회, 생성, 삭제, 수정 구현

--- src/main/java/com/sparta/springnewsfeed/service/PostService.java
+++ src/main/java/com/sparta/springnewsfeed/service/PostService.java
@@ -1,5 +1,8 @@
 package com.sparta.springnewsfeed.service;
 
+import com.sparta.springnewsfeed.dto.PostRequestDto;
+import com.sparta.springnewsfeed.dto.PostResponseDto;
+import com.sparta.springnewsfeed.entity.Post;
 import com.sparta.springnewsfeed.repository.PostRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +12,36 @@ public class PostService {
 
     public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
+    }
+
+    public PostResponseDto findById(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new NullPointerException("해당 게시물이 존재하지 않습니다.")
+        );
+        return new PostResponseDto(post);
+    }
+
+    public PostResponseDto createPost(PostRequestDto requestDto) {
+        Post post = new Post(requestDto);
+        Post savePost = postRepository.save(post);
+
+        return new PostResponseDto(savePost);
+    }
+
+    public PostResponseDto updateById(Long postId, PostRequestDto requestDto) {
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new NullPointerException("해당 게시물이 존재하지 않습니다.")
+        );
+        post.setTitle(requestDto.getTitle());
+        post.setContent(requestDto.getContent());
+        Post savePost = postRepository.save(post);
+        return new PostResponseDto(savePost);
+    }
+
+    public void deleteById(Long postId){
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new NullPointerException("해당 게시물이 존재하지 않습니다.")
+        );
+        postRepository.delete(post);
     }
 }
