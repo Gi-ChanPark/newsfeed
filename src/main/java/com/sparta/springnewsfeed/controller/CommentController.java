e22d240509391f75ca63dd870aba7482c4abe0c8
Woojin1123 <boom175839@gmail.com>
2024-09-04 20:30:27 +0900

    Fix : Comment Url 수정

--- src/main/java/com/sparta/springnewsfeed/controller/CommentController.java
+++ src/main/java/com/sparta/springnewsfeed/controller/CommentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/post/{postId}")
+@RequestMapping("/api/posts/{postId}")
 public class CommentController {
 
     private final CommentService commentService;
