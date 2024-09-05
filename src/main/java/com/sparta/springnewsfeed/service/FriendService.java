0ac6d33858cb9748f737ab1c201fb68a2f730c24
dam <ekadl025@gmail.com>
2024-09-05 18:15:56 +0900

    Fix : FriendService // 에러 응답 코드 설정

--- src/main/java/com/sparta/springnewsfeed/service/FriendService.java
+++ src/main/java/com/sparta/springnewsfeed/service/FriendService.java
@@ -9,8 +9,11 @@ import com.sparta.springnewsfeed.repository.FriendRepository;
 import com.sparta.springnewsfeed.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,28 +36,28 @@ public class FriendService {
 
     @Transactional
     public void friendAddRequest(Long userId, FriendAddRequest friendAddRequest) {
-        User fromUser = userRepository.findById(userId).orElseThrow(() -> new NoSuchElementException("사용자를 찾지 못했습니다."));
+        User fromUser = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"사용자를 찾지 못했습니다."));
         User toUser = userRepository.findByEmailAndNickname(friendAddRequest.getEmail(), friendAddRequest.getNickname());
 
         if(fromUser.equals(toUser)){
             log.error("자신에게는 친구 요청을 보내지 못합니다.");
-            throw new IllegalArgumentException("자신에게는 친구 요청을 보내지 못합니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"자신에게는 친구 요청을 보내지 못합니다.");
         }
 
         if(friendRepository.existsByToUser(fromUser) && friendRepository.existsByFromUser(toUser)){
             Friend friend = friendRepository.findByFromUserAndToUser(toUser, fromUser);
             if(friend.getStatus().equals(friendAcceptStatus)) {
                 log.error("친구입니다");
-                throw new IllegalArgumentException("친구입니다.");
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "친구입니다.");
             }else if(friend.getStatus().equals(friendWaitStatus)){
                 log.error("요청을 받았습니다.");
-                throw new IllegalArgumentException("요청을 받았습니다.");
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "요청을 받았습니다.");
             }
         }
 
         if(toUser == null) {
             log.error("사용자를 찾지 못했습니다.");
-            throw new NullPointerException("사용자를 찾지 못했습니다.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자를 찾지 못했습니다.");
         }
 
 
@@ -62,10 +65,10 @@ public class FriendService {
         if(friend2 != null){
             if(friend2.getStatus().equals(friendAcceptStatus)) {
                 log.error("친구입니다.");
-                throw new IllegalArgumentException("친구입니다.");
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "친구입니다.");
             }else if(friend2.getStatus().equals(friendWaitStatus)){
                 log.error("요청을 보냈습니다.");
-                throw new IllegalArgumentException("요청을 보냈습니다.");
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "요청을 보냈습니다.");
             }
         }
 
@@ -76,12 +79,12 @@ public class FriendService {
     }
 
     public List<FriendRequestListResponse> friendRequestList(Long userId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new NoSuchElementException("사용자를 찾지 못했습니다."));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자를 찾지 못했습니다."));
         List<Friend> friendList = friendRepository.findAllByToUserAndStatus(user, friendWaitStatus);
 
         if(friendList.isEmpty()){
             log.error("친구 요청이 없습니다.");
-            throw new NoSuchElementException("친구 요청이 없습니다.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "친구 요청이 없습니다.");
         }
 
         return friendList.stream().map(friend ->
@@ -89,11 +92,11 @@ public class FriendService {
     }
 
     public List<UserSearchFriendResponse> userSearchFriend(Long userId, UserSearchFriendRequest request) {
-        User fromUser = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("사용자를 찾지 못했습니다."));
+        User fromUser = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"사용자를 찾지 못했습니다."));
 
         List<User> searchUser = userRepository.findByNicknameContaining(request.getNickname());
         if(searchUser.isEmpty()) {
-            throw new NoSuchElementException("없는 이름입니다.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "없는 이름입니다.");
         }
 
         List<UserSearchFriendResponse> responseList = new ArrayList<>();
@@ -140,16 +143,16 @@ public class FriendService {
 
     @Transactional
     public void deleteFriend(Long userId, Long friendId) {
-        Friend friend = friendRepository.findById(friendId).orElseThrow(() -> new NoSuchElementException("사용자를 찾지 못했습니다."));
+        Friend friend = friendRepository.findById(friendId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자를 찾지 못했습니다."));
 
         if(!friend.getFromUser().getId().equals(userId) && !friend.getToUser().getId().equals(userId)){
             log.error("친구가 아닙니다.");
-            throw new IllegalArgumentException("친구가 아닙니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "친구가 아닙니다.");
         }
 
         if(friend.getStatus().equals(friendWaitStatus) || !friend.getStatus().equals(friendAcceptStatus)){
             log.error("친구가 아닙니다.");
-            throw new IllegalArgumentException("친구가 아닙니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "친구가 아닙니다.");
         }
 
         friendRepository.delete(friend);
@@ -157,11 +160,11 @@ public class FriendService {
 
     @Transactional
     public String friendAcceptanceRejection(Long userId, Long friendId, FriendRequestStatus status) {
-        Friend friend = friendRepository.findById(friendId).orElseThrow(() -> new NoSuchElementException("사용자를 찾지 못했습니다."));
+        Friend friend = friendRepository.findById(friendId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자를 찾지 못했습니다."));
 
         if(!friend.getToUser().getId().equals(userId)){
             log.error("받은 요청이 없습니다.");
-            throw new IllegalArgumentException("받은 요청이 없습니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "받은 요청이 없습니다.");
         }
 
             if (status.getStatus().equals(friendAcceptStatus)) {
@@ -174,6 +177,6 @@ public class FriendService {
             }
 
             log.error("잘못된 친구 요청입니다.");
-            throw new IllegalArgumentException("잘못된 친구 요청입니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"잘못된 친구 요청입니다.");
     }
 }
