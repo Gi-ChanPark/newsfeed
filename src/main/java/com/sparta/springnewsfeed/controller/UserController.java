1ac7fb74aff1d4e5a34f62651355df9a3b95950e
Gi-ChanPark <parkland0859@gmail.com>
2024-09-05 17:23:22 +0900

    fix : UserController, UserService 수정

--- src/main/java/com/sparta/springnewsfeed/controller/UserController.java
+++ src/main/java/com/sparta/springnewsfeed/controller/UserController.java
@@ -1,16 +1,13 @@
 package com.sparta.springnewsfeed.controller;
 
+import com.sparta.springnewsfeed.annotation.Auth;
 import com.sparta.springnewsfeed.config.JwtUtil;
 import com.sparta.springnewsfeed.dto.*;
-import com.sparta.springnewsfeed.entity.User;
 import com.sparta.springnewsfeed.repository.UserRepository;
 import com.sparta.springnewsfeed.service.UserService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,38 +32,41 @@ public class UserController {
         return ResponseEntity.ok(responseDto);
     }
 
+
     // 비밀번호 수정
     @PutMapping("/users/{userId}")
-    public ResponseEntity<UserPasswordUpdateResponseDto> updatePassword(@RequestHeader("Authorization") String token,
+    public ResponseEntity<UserPasswordUpdateResponseDto> updatePassword(@Auth AuthUser user,
                                                                         @PathVariable Long userId,
                                                                         @RequestBody UserPasswordUpdateRequestDto requestDto) {
-        UserPasswordUpdateResponseDto responseDto = userService.updatePassword(token, requestDto);
+        UserPasswordUpdateResponseDto responseDto = userService.updatePassword(user.getId(), userId,requestDto);
         return ResponseEntity.ok(responseDto);
     }
 
+
     // 유저 조회
     @GetMapping("/users")
-    public ResponseEntity<UserRequestDto> getUser(@RequestHeader("Authorization") String token) {
-        UserRequestDto responseDto = userService.getUser(token);
+    public ResponseEntity<UserRequestDto> getUser(@Auth AuthUser user) {
+        UserRequestDto responseDto = userService.getUser(user.getId());
         return ResponseEntity.ok(responseDto);
 
     }
 
     // 소개 수정
     @PutMapping("/users/{userId}/profile")
-    public ResponseEntity<UserIntroduceUpdateResponseDto> updateIntroduce(@RequestHeader("Authorization") String token,
+    public ResponseEntity<UserIntroduceUpdateResponseDto> updateIntroduce(@Auth AuthUser user,
                                                                           @PathVariable Long userId,
                                                                           @RequestBody UserIntroduceUpdateRequestDto requestDto) {
-        UserIntroduceUpdateResponseDto responseDto = userService.updateIntroduce(token, requestDto);
+        UserIntroduceUpdateResponseDto responseDto = userService.updateIntroduce(user.getId(), userId, requestDto);
         return ResponseEntity.ok(responseDto);
     }
 
     // 회원 탈퇴
     @DeleteMapping("/users/{userId}")
-    public ResponseEntity<String> deleteUser(@RequestHeader("Authorization") String token, @PathVariable Long userId) {
-        userService.deleteUser(token, userId);
+    public ResponseEntity<String> deleteUser(@Auth AuthUser user,
+                                             @PathVariable Long userId,
+                                             @RequestBody String enteredPassword) {
+        userService.deleteUser(user.getId(), userId, enteredPassword);
         return ResponseEntity.ok("회원 탈퇴가 완료되었습니다.");
     }
-
 
 }
